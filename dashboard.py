7e9dc5b847d3624f32741565800c840ec272b377
joshlo39 <83348928+joshlo39@users.noreply.github.com>
2023-12-17 15:54:15 -0500

    updated student name to a selectbox

--- dashboard.py
+++ dashboard.py
@@ -73,8 +73,31 @@ def main():
                                     ,value = default_URL)
     spreadsheet_id = spreadsheet_url.split('/')[-2]
     #---------------Sidebar----------------#
+    # student_names = sheets_service.spreadsheets().get(spreadsheetId=spreadsheet_id, ranges="Templates!A2:A2", includeGridData=False).execute()
+    # List of names
+    names = [
+        "Adriana Vingerhoets", "Aanya Naipaul", "Adriana Vingerhoets", "Alejandro Moreno",
+        "Andrea Murzi", "Andres Camargo", "Angelo Socarras", "Antonio De Castilho",
+        "Arielle Socol", "Bella Morales", "Brigida Sarcona", "Camila Moran",
+        "Cristina Macias", "Daisy Stein", "Daniel Esparragoza", "Daniel Sutton",
+        "Daniela Duran", "Daniela Padron", "Daniella Vargas", "David Ramirez",
+        "Diego Gutierrez", "Dylan Medina", "Eden Ohayon", "Elizabeth Chinea",
+        "Emilia Sarcona", "Esteban Chiquito", "Fabiana Tejera", "Freddy Romero",
+        "Gabriela Sobrinho", "Gabriella Alfonso", "Gabriella Suao", "Giovanna Musiello",
+        "Isabella Torres", "Jade Shim-You", "Jett Pinkerton", "Jielianne Rodriguez",
+        "Juan Rodriguez", "Julia Tavares", "Justin Cardenas", "Kirsten Chong",
+        "Lauren Kettlewell", "Liam Lesentier", "Lily Quintero", "Lola Shaoul",
+        "Lucas Delgado", "Melany Rodriguez", "Mia Vasquez", "Michelle Mendez",
+        "Natalie Fernandez", "Nathaly Gonzalez Henriquez", "Nicholas Santiago",
+        "Nicolas Alvarez", "Niya Bourdon", "Noah Gomberg", "Ofri Ezra", "Paula Cohen",
+        "Paulina Baquero", "Roberto Gamarra", "Rocket Pinkerton", "Sabrina Bianco",
+        "Sofia Aleman", "Sol Pereyra Lopez", "Sophia Loszynski", "Sydney Young",
+        "Thomas Dos Santos Lara", "Timothy Kinigopoulo", "Tom Sela", "Valerie Duran",
+        "Renan De Souza", "Zoe Mcmahon"
+    ]
+
     with st.sidebar:
-        student_name = st.text_input("Enter student name:")
+        student_name = st.selectbox("Select a student name:", names)
         if student_name:
             if st.button("Update Cell"):
                 if update_sheet_cell(sheets_service, spreadsheet_id, "Dashboard!A1", student_name):
