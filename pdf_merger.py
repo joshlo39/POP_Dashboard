7e4fa5492ac00949b696793f06db8c35a4718900
joshlo39 <83348928+joshlo39@users.noreply.github.com>
2024-02-05 09:54:35 -0500

    removing merged.pdf from save_dir

--- pdf_merger.py
+++ pdf_merger.py
@@ -228,6 +228,7 @@ def pdf_merger():
         image_paths = []
         inner_array = []
         file_names = os.listdir(save_dir)
+        file_names.remove('merged.pdf')
         print(f"File Names: {file_names}")
         file_names.sort(key = lambda x: int(x.split('_')[1].split('.')[0]))
         get_rick_rolled= "https://www.youtube.com/watch?v=v7ScGV5128A"
